0d5aa53f26f75be6cc5f3e1f8cd85f77a4948764
To-om <toom@thehive-project.org>
2017-10-17 15:17:17 +0200

    beautify the tests

--- test/controllers/UploadControllerSpec.scala
+++ test/controllers/UploadControllerSpec.scala
@@ -1,15 +1,16 @@
 package controllers
 
 import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
 
 import play.api.Application
 import play.api.http.Status
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
+import play.api.mvc.{ ControllerComponents, PlayBodyParsers }
 import play.api.mvc.MultipartFormData.FilePart
-import play.api.mvc._
 import play.api.test.Helpers.stubControllerComponents
 
 import akka.actor.ActorSystem
@@ -20,8 +21,8 @@ import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 
 
-class UploadTest extends PlaySpec with GuiceOneServerPerSuite with Status {
-  implicit val system: ActorSystem = ActorSystem("raw-body-parser-spec")
+class UploadControllerSpec extends PlaySpec with GuiceOneServerPerSuite with Status {
+  implicit val system: ActorSystem = ActorSystem("upload-file-test")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   def controllerComponents: ControllerComponents = {
@@ -34,27 +35,32 @@ class UploadTest extends PlaySpec with GuiceOneServerPerSuite with Status {
       .overrides(bind[ControllerComponents].toInstance(controllerComponents))
       .build()
 
-  "Uploading a small file should not return an error" in {
-    val wsClient = app.injector.instanceOf[WSClient]
-    val uploadURL = s"http://localhost:$port/upload"
+  val wsClient: WSClient = app.injector.instanceOf[WSClient]
+  val uploadURL = s"http://localhost:$port/upload"
 
-    val fakeData = Source.repeat(ByteString("ABCD")).take(4)
+  def generateData(size: Int): Source[ByteString, _] = {
+    val buffer = Array.ofDim[Byte](size)
+    Random.nextBytes(buffer)
+    Source.single(ByteString(buffer))
+  }
+
+  "Uploading a small file should not return an error" in {
+
+    val body = Source(FilePart("hello", "hello.txt", Option("text/plain"), generateData(16)) :: Nil)
     val response = Await.result(wsClient.url(uploadURL)
-      .post(Source(FilePart("hello", "hello.txt", Option("text/plain"), fakeData) :: Nil)), 5.seconds)
+      .post(body), 5.seconds)
 
     response.status mustBe OK
     response.body mustBe "hello.txt:16"
   }
 
   "Uploading a too large file should return an internal server error" in {
-    val wsClient = app.injector.instanceOf[WSClient]
-    val uploadURL = s"http://localhost:$port/upload"
 
-    val fakeData = Source.repeat(ByteString("ABCD")).take(1024)
+    val body = Source(FilePart("hello", "hello.txt", Option("text/plain"), generateData(4096)) :: Nil)
     val response = Await.result(wsClient.url(uploadURL)
-      .post(Source(FilePart("hello", "hello.txt", Option("text/plain"), fakeData) :: Nil)), 5.seconds)
+      .post(body), 5.seconds)
 
     response.status mustBe INTERNAL_SERVER_ERROR
-    //response.body mustBe "hello.txt:4096"
+    //response.body mustNot equal ("hello.txt:4096")
   }
 }
